56768465a0feb5aa8c572e08a1eb2f90ab68fbda
Somkene Ojukwu <somkeneoj@gmail.com>
2024-09-19 21:29:44 +0100

    2: auth functions implemented

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import handleAsyncErr from '../utils/catchAsync.js'
 const router = express.Router();
-import { signInHomeOwner, signUpHomeOwner, logOutHomeOwner, refreshToken } from '../controllers/userAuth.controller.js';
+import { signInHomeOwner, signUpHomeOwner, signUpInteriorDesigner, logOutHomeOwner, refreshToken } from '../controllers/userAuth.controller.js';
 import { signInLimiter, signUpLimiter } from '../middleware/rateLimiter.js';
 
-// /Signup post route
-router.post("/signup", signUpLimiter, handleAsyncErr(signUpHomeOwner));
+// /Signup post route for home owner
+router.post("/home-owner/signup", signUpLimiter, handleAsyncErr(signUpHomeOwner));
+
+
+// /Signup post route for designer
+router.post("/designer/signup", signUpLimiter, handleAsyncErr(signUpInteriorDesigner));
+
 
 // POST /login route
 router.post("/signin", signInLimiter, handleAsyncErr(signInHomeOwner));
