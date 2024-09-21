e0712d699beead6405d131ea6f38378a046d09be
Somkene Ojukwu <somkeneoj@gmail.com>
2024-09-21 18:16:40 +0100

    model updated

--- src/controllers/userAuth.controller.js
+++ src/controllers/userAuth.controller.js
@@ -52,7 +52,7 @@ export const signUpHomeOwner = async (req, res) => {
 
 // Sign Up Controller Function
 export const signUpInteriorDesigner = async (req, res) => {
-    const { fullname, email, password, portfolio,about,specialization } = req.body;
+    const { fullname, email, password, portfolio, about, specialization } = req.body;
 
     // Check if homeOwner or interiorDesigner already exists
     const existingHomeOwner = await homeOwner.findOne({ email });
--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,15 +1,27 @@
 import cors from 'cors';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+
 // Load environment variables from .env file
 dotenv.config();
 
-
 // Create and export the CORS middleware configuration
+const allowedOrigins = ['http://localhost:3000', 'https://decorvista.vercel.app'];
+
 const corsMiddleware = cors({
-  origin: 'http://localhost:3000' || 'https://decorvista.vercel.app', // Replace with your frontend URL
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+
+    // Check if the incoming origin is in the allowed origins array
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   credentials: true, // Allow cookies to be sent
-  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
 });
 
 export default corsMiddleware;
