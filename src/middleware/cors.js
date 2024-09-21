3f99c116899c292c89e466d74c1550c985e6de41
Somkene Ojukwu <somkeneoj@gmail.com>
2024-09-21 17:57:41 +0100

    model updated

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 // Create and export the CORS middleware configuration
 const corsMiddleware = cors({
-  origin: '*', // Replace with your frontend URL
+  origin: 'http://localhost:3000' || 'https://decorvista.vercel.app', // Replace with your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   credentials: true, // Allow cookies to be sent
   optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
